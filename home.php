593e80079afe48a892244d632f288eb747450fea
rialfu <remasertu@gmail.com>
2019-03-12 18:12:33 +0700

    tambahan

---
+++ home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="home.css">
+    <title>Course Javascript</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <a class="navbar-brand text-dark" href="#">Braike Rema</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link text-dark" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">About Me</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="awal">
+    <div class="container">
+    <h1 class="text-center text-dark">Hello Everybody</h1>
+    <h2 class="text-center text-dark">Hello my Name is Rema <br>nice to meet you</h2>
+        <div class="d-flex justify-content-center tombol">
+            <button class=" tm-a btn btn-lg btn-outline-dark text-dark about">About Me</button>
+            <button class="tm-a btn btn-lg btn-outline-dark text-dark">Project</button>
+        </div>
+    </div>
+</div>
+<div id="about">
+    <h2 class="text-center">About Me</h2>
+    <div class="container dua">
+        <div class="row">
+            <div class="col-sm-6 ">
+                <div class="image">
+                    <img src="communication.png" class="pic"alt="">
+                </div>
+                
+                <p class="bio">Name : Braike Rema Alfian M. <br>
+                Age : 20 <br>
+                University : Gunadarma
+                </p>
+            </div>
+            <div class="col-sm-6">
+                <div class="image">
+                    <img src="layout.png" class="pic"alt="">
+                </div>
+                <h4>Im Backend Developer</h4>
+                <p>I like because it's interesting</p>
+                <h4>Language</h4>
+                <p>PHP, Javascript,Java</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer">Created by <strong>Braike Rema</strong>.</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="home.js"></script>
+
+</body>
+</html>
